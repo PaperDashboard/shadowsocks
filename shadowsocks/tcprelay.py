f23eaa320a81ca94f5b0b228f77f10a15ad0a76e
Indexyz <jiduye@gmail.com>
2017-11-29 22:17:23 +0800

    fix: fix signal port speed limit(ref: https://github.com/esdeathlove/shadowsocks/pull/55)

--- shadowsocks/tcprelay.py
+++ shadowsocks/tcprelay.py
@@ -1779,10 +1779,17 @@ class TCPRelay(object):
                     self.multi_user_table[id][
                         '_forbidden_portset'] = PortRange(str(""))
 
-        if 'node_speedlimit' not in config or 'users_table' in self._config:
-            self.bandwidth = 0
-        else:
-            self.bandwidth = float(config['node_speedlimit']) * 128
+        # if 'node_speedlimit' not in config or 'users_table' in self._config:
+        #     self.bandwidth = 0
+        # else:
+        #     self.bandwidth = float(config['node_speedlimit']) * 128
+
+        # see also: https://github.com/esdeathlove/shadowsocks/pull/55
+        if not hasattr(self, 'bandwidth') or self.bandwidth == 0:
+            if 'node_speedlimit' not in config:
+                self.bandwidth = 0
+            else:
+                self.bandwidth = float(config['node_speedlimit']) * 128
 
         self.speed_tester_u = SpeedTester(self.bandwidth)
         self.speed_tester_d = SpeedTester(self.bandwidth)
